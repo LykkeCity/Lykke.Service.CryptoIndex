d5c75c7af5b8bc7bf10ef6f47031b6a1d4abb224
Alexey Zelenin <alexey@zelenin.org>
2018-10-23 10:50:30 +0300

    Fixed CoinMarcetCap model

--- Lykke.CoinMarketCap.Client/Models/CryptoCurrency/Quote.cs
+++ Lykke.CoinMarketCap.Client/Models/CryptoCurrency/Quote.cs
@@ -12,13 +12,13 @@ namespace Lykke.CoinMarketCap.Client.Models.CryptoCurrency
         public double Volume24H { get; set; }
 
         [JsonProperty("percent_change_1h")]
-        public double PercentChange1H { get; set; }
+        public double? PercentChange1H { get; set; }
 
         [JsonProperty("percent_change_24h")]
-        public double PercentChange24H { get; set; }
+        public double? PercentChange24H { get; set; }
 
         [JsonProperty("percent_change_7d")]
-        public double PercentChange7D { get; set; }
+        public double? PercentChange7D { get; set; }
 
         [JsonProperty("market_cap")]
         public long MarketCap { get; set; }
