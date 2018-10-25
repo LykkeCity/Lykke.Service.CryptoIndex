bd1cb316e0485bd44edd9f1763bc3bcc09b6fe38
Alexey Zelenin <alexey@zelenin.org>
2018-10-25 12:59:21 +0300

    Prevent to create an index state with 0 value

--- src/Lykke.Service.CryptoIndex.Domain/Models/LCI10/IndexState.cs
+++ src/Lykke.Service.CryptoIndex.Domain/Models/LCI10/IndexState.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Lykke.Service.CryptoIndex.Domain.Models.LCI10
 {
@@ -10,8 +12,8 @@ namespace Lykke.Service.CryptoIndex.Domain.Models.LCI10
 
         public IndexState(decimal value, IDictionary<string, decimal> middlePrices)
         {
-            Value = value;
-            MiddlePrices = middlePrices;
+            Value = value == default(decimal) ? throw new ArgumentOutOfRangeException(nameof(value)) : value;
+            MiddlePrices = !middlePrices.Any() ? throw new ArgumentOutOfRangeException(nameof(middlePrices)) : middlePrices;
         }
 
         public override string ToString()
