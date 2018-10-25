ee1a2f810e2cb1cd105b5ab7d8b799ac6fa6f0d1
Alexey Zelenin <alexey@zelenin.org>
2018-10-25 13:32:14 +0300

    Added cache for 3 sec for public api

--- src/Lykke.Service.CryptoIndex/Controllers/PublicController.cs
+++ src/Lykke.Service.CryptoIndex/Controllers/PublicController.cs
@@ -26,6 +26,7 @@ namespace Lykke.Service.CryptoIndex.Controllers
 
         [HttpGet("indices")]
         [ProducesResponseType(typeof(IReadOnlyList<(DateTime, decimal)>), (int)HttpStatusCode.OK)]
+        [ResponseCache(Duration = 3, VaryByQueryKeys = new[] { "*" })]
         public async Task<IReadOnlyList<(DateTime, decimal)>> GetIndexHistoriesAsync(DateTime from, DateTime to)
         {
             var domain = await _indexHistoryRepository.GetAsync(from, to);
@@ -37,6 +38,7 @@ namespace Lykke.Service.CryptoIndex.Controllers
 
         [HttpGet("indices/upToDate")]
         [ProducesResponseType(typeof(IReadOnlyList<(DateTime, decimal)>), (int)HttpStatusCode.OK)]
+        [ResponseCache(Duration = 3, VaryByQueryKeys = new[] { "*" })]
         public async Task<IReadOnlyList<(DateTime, decimal)>> GetIndexHistoriesAsync(DateTime to, int limit)
         {
             var result = await _indexHistoryRepository.GetUpToDateAsync(to, limit);
@@ -46,6 +48,7 @@ namespace Lykke.Service.CryptoIndex.Controllers
 
         [HttpGet("index/current")]
         [ProducesResponseType(typeof((DateTime, decimal)), (int)HttpStatusCode.OK)]
+        [ResponseCache(Duration = 3, VaryByQueryKeys = new[] { "*" })]
         public async Task<(DateTime, decimal)> GetCurrentAsync()
         {
             var result = (await _indexHistoryRepository.TakeLastAsync(1)).SingleOrDefault();
